627c2bde825073f39931fec06f473c57cca14cb2
KARLA KRISTINA DE CASTRO RODRIGUES <karla_k_rodrigues@estudante.sesisenai.org.br>
2025-05-15 16:38:53 -0300

    Arquivos do eclipse

---
+++ projetoUm/src/projetoUm2/senai.java
@@ -0,0 +1,21 @@
+package projetoUm2;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class senai {
+
+	public static void main(String[] args) {
+		
+String nomePessoa;
+double salario;
+int tempodeEmpresa;
+
+Scanner nome = new Scanner(System.in);
+System.out.println("Digite seu nome");
+nomePessoa = nome.nextLine();
+System.out.println();
+
+	}
+
+}
